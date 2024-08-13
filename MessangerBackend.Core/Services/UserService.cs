b078caaa88685fdf205e0bb4c6e3ba12164339da
Artem <artem.nakhman@gmail.com>
2024-08-13 16:15:19 +0300

    Messager hw07_08_2024

---
+++ MessangerBackend.Core/Services/UserService.cs
@@ -0,0 +1,56 @@
+﻿using MessangerBackend.Core.Interfaces;
+using MessangerBackend.Core.Models;
+
+namespace MessangerBackend.Core.Services;
+
+public class UserService : IUserService
+{
+    private readonly IRepository _repository;
+
+    public UserService(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<User> Login(string nickname, string password)
+    {
+        return (await _repository.GetQuery<User>(u => u.Nickname == nickname && u.Password == password)).SingleOrDefault();
+     }
+
+    public Task<User> Register(string nickname, string password)
+    {
+        var User = _repository.GetQuery<User>(u => u.Nickname == nickname && u.Password==password);
+        if (User != null)
+            return null;
+
+        User newUser = new User()
+        {
+            Nickname = nickname,
+            Password = password,
+            CreatedAt = DateTime.Now,
+            LastSeenOnline = DateTime.Now
+        };
+
+
+        return _repository.Add(newUser);
+    }
+
+    public Task<User> GetUserById(int id)
+    {
+        return _repository.GetById<User>(id);
+    }
+
+    public IEnumerable<User> GetUsers(int page, int size)
+    {
+        if (page <= 0 || size <= 0)
+          return Enumerable.Empty<User>();
+
+      return (_repository.GetQuery<User>(u => true).Result).Skip((page - 1) * size).Take(size).ToList();
+    }
+
+
+    public IEnumerable<User> SearchUsers(string nickname)
+    {
+        return (IEnumerable<User>)_repository.GetQuery<User>(u => u.Nickname == nickname);
+    }
+}
